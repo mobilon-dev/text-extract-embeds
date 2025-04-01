ffe1b12f124d02c7f34fc05b192bcafedd42dc8a
Chumakov Rustam <rusacuma306@gmail.com>
2025-04-01 12:33:01 +0700

    fix: clean up

--- src/embed_analyze.ts
+++ src/embed_analyze.ts
@@ -1,4 +1,4 @@
-import {MatchResult, PLATFORM_REGEX} from './data'
+import {MatchResult} from './data'
 import {Analyzer} from './analyzer'
 import {
     YoutubeStrategy,
@@ -28,37 +28,6 @@ export function analyzeEmbed(data: string): MatchResult[] {
     if (vkVideoMatches) results.push(...vkVideoMatches);
     const yandexMusicMatches = analyzer.analyze("yandex_music", data);
     if (yandexMusicMatches) results.push(...yandexMusicMatches);
-    /*
-    for (const [type, regexString] of Object.entries(PLATFORM_REGEX)) {
-        const regex = new RegExp(regexString, 'g');
-        let match: RegExpExecArray | null;
-
-        while ((match = regex.exec(data)) !== null) {
-            const fullMatch = match[0];
-            let videoId: string | null = match[1] || null;
-
-            // Handle VK video ID specifically.  The regex now captures both parts.
-            if (type === 'VK') {
-                videoId = `${match[1]}_${match[2]}`;
-            }
-
-
-            results.push({
-                type,
-                videoId: videoId,
-                url: fullMatch,
-                index: match.index,
-            });
-        }
-    }
-    */
 
     return results;
 }
-
-// Пример использования:
-/*
-const inputString = 'Привет. Вот видео с котами https://www.youtube.com/watch?v=JxS5E-kZc2s и плейлист:https://www.youtube.com/watch?v=yRfKToBbdOc     https://www.youtube.com/watch?v=OWXMsL_JnQQ  А вот видео Rutube: https://rutube.ru/video/b1c6f3a861e2e4f84179ba2f92521a57/  и vk: https://vk.com/video-45256126_456242536';
-const analysis = analyzeEmbed(inputString);
-console.log(analysis);
-*/
